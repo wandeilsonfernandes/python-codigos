ab03cf8e645e58208ff2dc3fe7754207951157da
Wandeilson Fernandes <wandeilsonfernandes2019@outlook.com>
2023-03-14 19:23:45 -0400

    quebrando-loops.py criado
    Usando break e continue

---
+++ quebrando-loops.py
@@ -0,0 +1,13 @@
+linguagens = [
+    'C++',
+    'Javascript',
+    'Python',
+    'Ruby',
+    'Rust',
+]
+
+for lingua in linguagens:
+    if lingua == 'Ruby':
+        continue
+    print(ling)
+print('feito')
